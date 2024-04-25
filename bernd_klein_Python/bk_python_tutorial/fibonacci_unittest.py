6aeee27dbc29eb24a60f72623d401d6315fb67b6
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2024-04-25 19:51:19 -0400

    Test

---
+++ bernd_klein_Python/bk_python_tutorial/fibonacci_unittest.py
@@ -0,0 +1,13 @@
+import unittest
+from fibonacci_doctest1 import fib
+
+class FibonacciTest(unittest.TestCase):
+    def testCalculation(self):
+        self.assertEqual(fib(0), 0)
+        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib(5), 5)
+        self.assertEqual(fib(10), 55)
+        self.assertEqual(fib(20), 6765)
+
+if __name__ == "main":
+    unittest.main()
